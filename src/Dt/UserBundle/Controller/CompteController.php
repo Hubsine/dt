c7bfc8a9a7183178b2fe8d3e51b5d34b8fe7fe62
Hubsine <contact@hubsine.com>
2017-08-03 02:41:17 +0200

    - remove edit qui suis je action

--- src/Dt/UserBundle/Controller/CompteController.php
+++ src/Dt/UserBundle/Controller/CompteController.php
@@ -61,11 +61,6 @@ class CompteController extends Controller
         return $this->render('DtUserBundle:Compte:ReseauxSociaux/show.html.twig');
     }
     
-    public function showQuiSuisJeAction(Request $request, User $user)
-    {
-        return $this->render('DtUserBundle:Compte:QuiSuisJe/show.html.twig');
-    }
-    
     public function showAboutUserReplyAction(Request $request, User $user){
         
         /** @var $aboutUserManager AboutUserManager */
@@ -133,55 +128,6 @@ class CompteController extends Controller
                 'form'  => $this->renderView($templateToEdit, array(
                     'form' => $form->createView(),
                     'aboutUsers'    => $aboutUsers
-                     ))
-            ), $codeResponse );
-        
-        return $response;
-        
-    }
-    
-    public function editQuiSuisJeAction(Request $request, User $user){
-        
-        $codeResponse = 200;
-        $contentId = 'quiSuisJeContent';
-        $templateToShow = 'DtUserBundle:Compte:QuiSuisJe/show.html.twig';
-        $templateToEdit = 'DtUserBundle:Compte:QuiSuisJe/edit.html.twig';
-        
-        $user = $this->getUser();
-        $form = $this->createForm(ReseauxSociauxFormType::class, $user);
-        
-        $form->handleRequest($request);
-        
-        if($form->isSubmitted()){
-            
-            if($form->isValid()){
-                
-                /** @var $userManager UserManagerInterface */
-                $userManager = $this->get('fos_user.user_manager');
-                
-                $userManager->updateUser($user);
-                
-                $message = $this->get('translator')->trans('change_profile.success',array(), 'FOSUserBundle');
-                
-                $response = new JsonResponse(array(
-                    'contentId' => $contentId,
-                    'form'  => $this->renderView($templateToShow, array(
-                        'message'   => $message
-                         ))
-                ), $codeResponse );
-
-                return $response;
-                
-            }else{
-                $codeResponse = 400;
-            }
-        }
-        
-        $response = new JsonResponse(
-            array(
-                'contentId' => $contentId,
-                'form'  => $this->renderView($templateToEdit, array(
-                    'form' => $form->createView(),
                      ))
             ), $codeResponse );
         

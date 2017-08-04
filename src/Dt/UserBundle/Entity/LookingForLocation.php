56724f58fa130aa3fae2f21e2b35ce9c6ea0ef1c
Hubsine <contact@hubsine.com>
2017-08-04 22:02:05 +0200

    - create Adress and LookingForLocation entitty with repository

---
+++ src/Dt/UserBundle/Entity/LookingForLocation.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Dt\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use ONGR\ElasticsearchBundle\Annotation as ES;
+use Symfony\Component\Validator\Constraints as Assert;
+use Gedmo\SoftDeleteable\Traits\SoftDeleteableEntity;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Dt\UserBundle\Entity\Adress;
+
+/**
+ * Localisation
+ *
+ * @ORM\Table(name="dt_localisation")
+ * @ORM\Entity(repositoryClass="Dt\UserBundle\Repository\LookingForLocationRepository")
+ * 
+ */
+class LookingForLocation extends Adress
+{
+
+    
+}
---
+++ src/Dt/UserBundle/Repository/LookingForLocationRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Dt\UserBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * LocalisationRepository
+ *
+ */
+class LocalisationRepository extends EntityRepository
+{
+}

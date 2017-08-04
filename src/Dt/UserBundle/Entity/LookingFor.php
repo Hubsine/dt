b4ae8c914c722357b6c203a0fd280f46ba8cd423
Hubsine <contact@hubsine.com>
2017-08-04 20:13:42 +0200

    - Add assert and orm - Add message validator

--- src/Dt/UserBundle/Entity/LookingFor.php
+++ src/Dt/UserBundle/Entity/LookingFor.php
@@ -35,15 +35,66 @@ class LookingFor {
     private $id;
     
     /**
+     * @var Dt\UserBundle\Entity\User
+     * 
+     * @ORM\OneToOne(targetEntity="Dt\UserBundle\Entity\User")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+    
+    /**
      * @var array
      * 
-     * ORM\Column(type="array", length=10, name="gender", nullable=true)
+     * @ORM\OneToMany(targetEntity="Dt\AdminBundle\Entity\LookingForMeta")
+     * @ORM\JoinColumn(nullable=false)
+     * 
+     * @Assert\Expression(
+     *      "value.getOnProperty() == 'gender'",
+     *      message="dt_looking_for.gender.expression"
+     * )
      */
     private $gender;
     
-    private $ageRange = array('min', 'max');
-    
+    /**
+     * @var type 
+     * 
+     * @ORM\OneToMany(targetEntity="Dt\AdminBundle\Entity\LookingForMeta")
+     * 
+     * @Assert\Expression(
+     *      "value.getOnProperty() == 'relationships'",
+     *      message="dt_looking_for.relationships.expression"
+     * )
+     */
     private $relationships;
+    
+    /**
+     * @var array
+     * 
+     * @ORM\Column(name="age_range", type="array")
+     * 
+     * @Assert\Collection(
+     *      fields={
+     *          "min" = @Assert\Optional(
+     *                              @Assert\Type(
+     *                                  type="integer", 
+     *                                  message="dt_looking_for.age_range.min.type",
+     *                              )
+     *          ),
+     *          "max" = @Assert\Optional(
+     *                              @Assert\Type(
+     *                                  type="integer", 
+     *                                  message="dt_looking_for.age_range.max.type",
+     *                              )
+     *          )
+     *      }
+     * )
+     * 
+     * @Assert\Expression(
+     *      "value.min <= value.max",
+     *      message="dt_looking_for.age_range.expression"
+     * )
+     */
+    private $ageRange = array('min' => 18, 'max' => 99);
     
     private $localisation;
     

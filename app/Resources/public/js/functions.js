0a7ea933135b365cd40bc566ddfd43d5ca2fe070
Hubsine <contact@hubsine.com>
2017-08-12 00:25:25 +0200

    - google maps api js autocomplete with region by country is ok

--- app/Resources/public/js/functions.js
+++ app/Resources/public/js/functions.js
@@ -1,7 +1,29 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+function autocompleteLocationRegion(selectCountry, inputRegion, autocompleteRegion)
+{
+    inputRegion.val('');
+    
+    autocompleteRegion.setComponentRestrictions({'country': selectCountry.val()});
+
+    google.maps.event.addListener(autocompleteRegion, 'place_changed', function () {
+
+        var place = autocompleteRegion.getPlace();
+        var regionTypes = ['administrative_area_level_1', 'administrative_area_level_2'];
+
+        // Is administrative_area_level_1 or administrative_area_level_2
+        if( place.types.length > 0 && 
+            ( 
+                $.inArray( regionTypes[0], place.types ) !== -1 
+                || $.inArray( regionTypes[1], place.types ) !== -1
+            ) 
+        )
+        {
+            inputRegion.val( place.name ).parent('.form-group').removeClass('has-error');
+        }else{
+            inputRegion.val('').parent('.form-group').addClass('has-error');
+        }
+        
+        console.log(place);
+    });
+}
 

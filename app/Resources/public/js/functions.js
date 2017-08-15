8bc75e01abc605b1bbb8841b899fae494ba23c2d
Hubsine <contact@hubsine.com>
2017-08-15 17:07:26 +0200

    - google map autocomplte ok

--- app/Resources/public/js/functions.js
+++ app/Resources/public/js/functions.js
@@ -1,6 +1,16 @@
 ////
 // Google Maps API JS - Autocomplete
 ////
+function autocompleteItemDisabled(element, disabled = true)
+{
+    if( disabled )
+    {
+        element.attr('disabled', 'disabled');
+    }else{
+        element.attr('disabled', false);
+    }
+}
+
 function autocompleteItemReset(element)
 {
     element.val('');
@@ -8,7 +18,8 @@ function autocompleteItemReset(element)
 
 function autocompleteSuccess(element, place)
 {
-    element.val( place.name ).parent('.form-group').removeClass('has-error');
+    //element.val( place.name ).parent('.form-group').removeClass('has-error');
+    element.parent('.form-group').removeClass('has-error');
 }
 
 function autocompleteError(element)
@@ -18,29 +29,55 @@ function autocompleteError(element)
 
 function autocompleteLocationRegion(selectCountry, inputRegion, autocompleteRegion)
 {
-    autocompleteItemReset(inputRegion)
+    autocompleteItemReset(inputRegion);
     
     autocompleteRegion.setComponentRestrictions({'country': selectCountry.val()});
 
-    google.maps.event.addListener(autocompleteRegion, 'place_changed', function () {
-
-        var place = autocompleteRegion.getPlace();
-        var regionTypes = ['administrative_area_level_1', 'administrative_area_level_2'];
-
-        // Is administrative_area_level_1 or administrative_area_level_2
-        if( place.types.length > 0 && 
-            ( 
-                $.inArray( regionTypes[0], place.types ) !== -1 
-                || $.inArray( regionTypes[1], place.types ) !== -1
-            ) 
-        )
-        {
-            autocompleteSuccess(inputRegion, place);
-        }else{
-            autocompleteError(inputRegion);
-        }
-        
-        console.log(place);
-    });
+//    google.maps.event.addListener(autocompleteRegion, 'place_changed', function () {
+//
+//        var place = autocompleteRegion.getPlace();
+//        var regionTypes = ['administrative_area_level_1', 'administrative_area_level_2'];
+//
+//        // Is administrative_area_level_1 or administrative_area_level_2
+//        if( place.types.length > 0 && 
+//            ( 
+//                $.inArray( regionTypes[0], place.types ) !== -1 
+//                || $.inArray( regionTypes[1], place.types ) !== -1
+//            ) 
+//        )
+//        {
+//            autocompleteSuccess(inputRegion, place);
+//        }else{
+//            autocompleteError(inputRegion);
+//        }
+//        
+//        //console.log(place);
+//    });
 }
 
+function autocompleteLocationCity(selectCountry, inputCity, autocompleteCity)
+{
+    autocompleteItemReset(inputCity);
+    
+    autocompleteCity.setComponentRestrictions({'country': selectCountry.val()});
+
+//    google.maps.event.addListener(autocompleteCity, 'place_changed', function () {
+//
+//        var place = autocompleteCity.getPlace();
+//        var cityTypes = ['locality'];
+//        
+//        // Is administrative_area_level_1 or administrative_area_level_2
+//        if( place.types.length > 0 && 
+//            ( 
+//                $.inArray( cityTypes[0], place.types ) !== -1 
+//            ) 
+//        )
+//        {
+//            autocompleteSuccess(inputCity, place);
+//        }else{
+//            autocompleteError(inputCity);
+//        }
+//        
+//        console.log(place);
+//    });
+}

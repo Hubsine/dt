710a7a405a40ba6bf266a3a7158fdb1e496a7b7c
Hubsine <contact@hubsine.com>
2017-08-17 22:19:48 +0200

    - delete id property and setter and getter

--- src/Dt/UserBundle/Entity/UserLocation.php
+++ src/Dt/UserBundle/Entity/UserLocation.php
@@ -13,23 +13,5 @@ use Dt\UserBundle\Entity\Address;
  */
 class UserLocation extends Address
 {
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    private $id;
-
-
-    /**
-     * Get id
-     *
-     * @return integer 
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
+    
 }

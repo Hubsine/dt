e779554e1d532e85fbee1d6889c061cde269e193
Hubsine <contact@hubsine.com>
2017-08-22 01:55:48 +0200

    - create base manager class

---
+++ src/AppBundle/Doctrine/BaseManager.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Doctrine;
+
+use AppBundle\Model\AbstractManager;
+use Doctrine\Common\Persistence\ObjectManager;
+
+/**
+ * Description of BaseManager
+ *
+ * @author Hubsine
+ */
+class BaseManager extends AbstractManager{
+    
+    public function __construct(ObjectManager $om) 
+    {
+        $this->objectManager = $om;
+    }
+    
+}

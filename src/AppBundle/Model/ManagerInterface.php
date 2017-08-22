30f9c5cad706eca54be9601b3a891616681cf590
Hubsine <contact@hubsine.com>
2017-08-22 15:03:22 +0200

    - change parameter $classname = null to $className

--- src/AppBundle/Model/ManagerInterface.php
+++ src/AppBundle/Model/ManagerInterface.php
@@ -12,7 +12,7 @@ use AppBundle\Doctrine\EntityInterface;
  */
 interface ManagerInterface {
 
-    public function __construct(ObjectManager $om, $className = null);
+    public function __construct(ObjectManager $om, $className);
 
     public function getRepository();
     

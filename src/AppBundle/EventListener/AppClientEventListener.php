2abdc9d5cff2ac8ad289bfcd84ea43d485664a2b
Hubsine <contact@hubsine.com>
2017-11-08 19:40:50 +0100

    - add function to check if user is online or not

--- src/AppBundle/EventListener/AppClientEventListener.php
+++ src/AppBundle/EventListener/AppClientEventListener.php
@@ -8,6 +8,7 @@ use Gos\Bundle\WebSocketBundle\Event\ServerEvent;
 use Gos\Bundle\WebSocketBundle\Event\ClientRejectedEvent;
 use Gos\Bundle\WebSocketBundle\Client\ClientManipulatorInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use React\Socket\ConnectionInterface;
 
 /**
  * Description of AppClientEventListener
@@ -93,10 +94,30 @@ class AppClientEventListener
      *
      * @param ClientRejectedEvent $event
      */
-	public function onClientRejected(ClientRejectedEvent $event)
+    public function onClientRejected(ClientRejectedEvent $event)
     {
     	$origin = $event->getOrigin();
 
-		echo 'connection rejected from '. $origin . PHP_EOL;
+	echo 'connection rejected from '. $origin . PHP_EOL;
+    }
+    
+    /**
+     * Check if user is online or not
+     * 
+     * @param UserInterface $user
+     * @return boolean
+     */
+    public function userIsOnline(UserInterface $user)
+    {
+        $userId = $user->getId();
+        $connection = ( isset($this->connections[$userId]) ) ? $this->connections[$userId] : null;
+        
+        if( $connection instanceof ConnectionInterface)
+        {
+            return true;
+        }
+        
+        return false;
+        
     }
 }
